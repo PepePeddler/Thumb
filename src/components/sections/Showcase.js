3d5785663a305344a6584a8bfcb5d1c54ee07651
Jake(PP) <100009806+PepePeddler@users.noreply.github.com>
2023-08-18 17:30:37 +0100

    Update Showcase.js

--- src/components/sections/Showcase.js
+++ src/components/sections/Showcase.js
@@ -1,16 +1,16 @@
 import React, { useRef } from 'react'
 import styled, { keyframes } from 'styled-components'
 
-import img1 from '../../assets/Nfts/bighead.svg';
-import img2 from '../../assets/Nfts/bighead-1.svg';
-import img3 from '../../assets/Nfts/bighead-2.svg';
-import img4 from '../../assets/Nfts/bighead-3.svg';
-import img5 from '../../assets/Nfts/bighead-4.svg';
-import img6 from '../../assets/Nfts/bighead-5.svg';
-import img7 from '../../assets/Nfts/bighead-6.svg';
-import img8 from '../../assets/Nfts/bighead-7.svg';
-import img9 from '../../assets/Nfts/bighead-8.svg';
-import img10 from '../../assets/Nfts/bighead-9.svg';
+import img1 from '../../assets/Nfts/meme-10.svg';
+import img2 from '../../assets/Nfts/meme-1.svg';
+import img3 from '../../assets/Nfts/meme-2.svg';
+import img4 from '../../assets/Nfts/meme-3.svg';
+import img5 from '../../assets/Nfts/meme-4.svg';
+import img6 from '../../assets/Nfts/meme-5.svg';
+import img7 from '../../assets/Nfts/meme-6.svg';
+import img8 from '../../assets/Nfts/meme-7.svg';
+import img9 from '../../assets/Nfts/meme-8.svg';
+import img10 from '../../assets/Nfts/meme-9.svg';
 import ETH from '../../assets/icons8-ethereum-48.png'
 
 const Section = styled.section`
