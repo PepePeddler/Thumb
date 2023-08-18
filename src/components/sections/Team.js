dd023e41ffac934036a5eb3f85739e8688c4486f
Jake(PP) <100009806+PepePeddler@users.noreply.github.com>
2023-08-18 17:31:17 +0100

    Update Team.js

--- src/components/sections/Team.js
+++ src/components/sections/Team.js
@@ -1,15 +1,15 @@
 import React, { lazy, Suspense } from 'react'
 import styled from 'styled-components';
 
-import img1 from '../../assets/Nfts/bighead.svg';
-import img2 from '../../assets/Nfts/bighead-1.svg';
-import img3 from '../../assets/Nfts/bighead-2.svg';
-import img4 from '../../assets/Nfts/bighead-3.svg';
-import img5 from '../../assets/Nfts/bighead-4.svg';
-import img6 from '../../assets/Nfts/bighead-5.svg';
-import img7 from '../../assets/Nfts/bighead-6.svg';
-import img8 from '../../assets/Nfts/bighead-7.svg';
-import img9 from '../../assets/Nfts/bighead-8.svg';
+import img1 from '../../assets/Nfts/meme-10.svg';
+import img2 from '../../assets/Nfts/meme-1.svg';
+import img3 from '../../assets/Nfts/meme-2.svg';
+import img4 from '../../assets/Nfts/meme-3.svg';
+import img5 from '../../assets/Nfts/meme-4.svg';
+import img6 from '../../assets/Nfts/meme-5.svg';
+import img7 from '../../assets/Nfts/meme-6.svg';
+import img8 from '../../assets/Nfts/meme-7.svg';
+import img9 from '../../assets/Nfts/meme-8.svg';
 import Loading from '../Loading';
 // import ConfettiComponent from '../Confetti';
 
