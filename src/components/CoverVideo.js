8af1cccae4066d2e9e0cd1d0c4a7124a17e30fbc
Jake(PP) <100009806+PepePeddler@users.noreply.github.com>
2023-08-20 14:47:23 +0100

    Update CoverVideo.js

--- src/components/CoverVideo.js
+++ src/components/CoverVideo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import GIF from '../assets/Home GIF.gif'
+import GIF from '../assets/Home Video.mp4'
 
 
 const VideoContainer = styled.div`
