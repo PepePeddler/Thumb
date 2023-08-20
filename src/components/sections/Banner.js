855663a21d11325091122a78e9ae414daa9e2f5f
Jake(PP) <100009806+PepePeddler@users.noreply.github.com>
2023-08-20 15:05:49 +0100

    Update Banner.js

--- src/components/sections/Banner.js
+++ src/components/sections/Banner.js
@@ -146,17 +146,17 @@ const Banner = () => {
   return (
     <Section>
         <ImgContainer>
-        <img src={img1} width="500" height="400"  alt="The Weirdos" />
-        <img src={img2} width="500" height="400"  alt="The Weirdos" />
-        <img src={img3} width="500" height="400"  alt="The Weirdos" />
-        <img src={img4} width="500" height="400"  alt="The Weirdos" />
-        <img src={img5} width="500" height="400"  alt="The Weirdos" />
-        <img src={img6} width="500" height="400"  alt="The Weirdos" />
+        <img src={img1} width="500" height="400"  alt="Me Mes" />
+        <img src={img2} width="500" height="400"  alt="Me Mes" />
+        <img src={img3} width="500" height="400"  alt="Me Mes" />
+        <img src={img4} width="500" height="400"  alt="Me Mes" />
+        <img src={img5} width="500" height="400"  alt="Me Mes" />
+        <img src={img6} width="500" height="400"  alt="Me Mes" />
 
         </ImgContainer>
         <Title>Join the
         <br/>
-        Weirdos Club
+        Me Mes
         </Title>
         <BtnContainer>
         <JoinNow>
